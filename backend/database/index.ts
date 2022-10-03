cd3caf5c0c896416e881c5c47eb89c2cf1eb8a94
simone-lofaso <99053610+simone-lofaso@users.noreply.github.com>
2022-10-03 12:53:10 -0700

    Set up database models and added diagram to README.md

--- backend/database/index.ts
+++ backend/database/index.ts
@@ -9,12 +9,51 @@ const connection = mysql.createConnection({
 
 connection.query(
     `CREATE TABLE IF NOT EXISTS users(
-        \`id\` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-        \`username\` VARCHAR(45) NOT NULL,
+        \`id\` INT NOT NULL AUTO_INCREMENT,
+        \`email\` VARCHAR(45) NOT NULL,
         \`password\` VARCHAR(45) NOT NULL,
+        \`tokens\` INT NOT NULL,
         PRIMARY KEY (\`id\`),
         UNIQUE INDEX \`id_UNIQUE\` (\`id\` ASC) VISIBLE,
-        UNIQUE INDEX \`username_UNIQUE\` (\`username\` ASC) VISIBLE
+        UNIQUE INDEX \`email_UNIQUE\` (\`email\` ASC) VISIBLE
     )`
 )
+
+connection.query(
+    `CREATE TABLE IF NOT EXISTS plates(
+        \`id\` INT NOT NULL AUTO_INCREMENT,
+        \`plate_num\` VARCHAR(45) NOT NULL,
+        \`user_id\` INT NOT NULL,
+        PRIMARY KEY (\`id\`),
+        UNIQUE INDEX \`id_UNIQUE\` (\`id\` ASC) VISIBLE,
+        FOREIGN KEY (\`user_id\`) REFERENCES users(\`id\`)
+    )`
+)
+
+connection.query(
+    `CREATE TABLE IF NOT EXISTS garages(
+        \`id\` INT NOT NULL AUTO_INCREMENT,
+        \`total_spaces\` INT NOT NULL,
+        \`occupied_spaces\` INT NOT NULL,
+        \`name\` VARCHAR(45) NOT NULL,
+        PRIMARY KEY (\`id\`),
+        UNIQUE INDEX \`id_UNIQUE\` (\`id\` ASC) VISIBLE
+    )`
+)
+
+connection.query(
+    `CREATE TABLE IF NOT EXISTS spaces(
+        \`id\` INT NOT NULL AUTO_INCREMENT,
+        \`floor\` INT NOT NULL,
+        \`row\` INT NOT NULL,
+        \`section\` CHAR NOT NULL,
+        \`garage_id\` INT NOT NULL,
+        \`parked_user\` INT NOT NULL,
+        PRIMARY KEY (\`id\`),
+        UNIQUE INDEX \`id_UNIQUE\` (\`id\` ASC) VISIBLE,
+        FOREIGN KEY (\`parked_user\`) REFERENCES users(\`id\`),
+        FOREIGN KEY (\`garage_id\`) REFERENCES garages(\`id\`)
+    )`
+)
+
 export {connection as db} 
