288ed481f42ae574d8607414ae9a6c521b490432
Logan Dang <logandang100@gmail.com>
2022-10-11 20:00:53 -0700

    format

--- backend/app.ts
+++ backend/app.ts
@@ -7,16 +7,14 @@ import UserRouter from './routes/user';
 db;
 const port = 8000;
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded());
 app.use('/api/v1/user/', UserRouter);
-
 app.get('/', (req, res) => {
   req;
   res.json({ message: 'Hello World!' });
 });
 
-app.listen(port, process.env.IPHOST || "localhost" , () => {
+app.listen(port, process.env.IPHOST || 'localhost', () => {
   console.log(`Listening on port: ${[port]}`);
 });
