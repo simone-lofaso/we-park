51a531164fa8aaa9afac66f5782ff0a90281cdb5
Rafapp <rpadiper@gmail.com>
2022-10-18 20:14:43 -0700

    Adding ScanAI screen
    Added PNG, SVG extensions. Added new screen along with its references. Will need Logan to add button for this screen so that we can test it. -Rafa

--- frontend/__tests__/StyledText.test.tsx
+++ frontend/__tests__/StyledText.test.tsx
@@ -1,8 +1,8 @@
 import { create } from 'react-test-renderer';
 import { MonoText } from '../components/StyledText';
+import {ScanAI} from '../screens/ScanAI';
 
 it(`renders correctly`, () => {
-  const tree = create(<MonoText>Snapshot test!</MonoText>).toJSON();
-
+  const tree = create(<ScanAI/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
