9dfca8b404855889789c091e29d14660d4ff70ae
simone-lofaso <99053610+simone-lofaso@users.noreply.github.com>
2022-10-25 23:39:30 -0700

    Abstract db (#35)
    * UserTable
    
    * more backend
    
    Co-authored-by: Logan Dang <logandang100@gmail.com>

--- backend/database/index.ts
+++ backend/database/index.ts
@@ -61,4 +61,29 @@ connection.query(
     )`
 );
 
+connection.query(
+  //use execute if user input, w/ ? instead of arr
+  `INSERT INTO spaces(floor, row, section, parked_user, garage_id) values (1, 1, 'A', null, 1), 
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1),
+  (1, 1, 'A', null, 1)`
+);
+
 export { connection as db };
