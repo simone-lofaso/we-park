0f6f1c74b860e2759e04a1a5a46560a022193e2b
simone-lofaso <99053610+simone-lofaso@users.noreply.github.com>
2022-10-25 20:50:51 -0700

    more backend

---
+++ backend/database/plates.ts
@@ -0,0 +1,16 @@
+/**
+ * Module representing the plates table
+ */
+
+import { db } from '.';
+
+const PlatesTable = {
+    register: (plate_num: string) => {
+        db.execute(
+            `INSERT INTO plates(plate_num) values '?');`,
+            [plate_num]
+        );
+    },
+    execute: db.execute,
+};
+export default PlatesTable;
