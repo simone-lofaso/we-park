7a3c9b78799ea85cde7a1253f069c58600d9f1c0
Rafapp <rpadiper@gmail.com>
2022-11-01 19:23:40 -0700

    added buttons

--- frontend/screens/Home.tsx
+++ frontend/screens/Home.tsx
@@ -8,8 +8,20 @@ export default function Home({ navigation }: RootStackScreenProps<'Home'>) {
   return (
     <SafeAreaView>
       <Button
-        title='Go To Recommend'
+        title='Go To Map'
         onPress={() => navigation.navigate('Map')}
+      />
+      <Button
+        title='Go To Register'
+        onPress={() => navigation.navigate('Register')}
+      />
+      <Button
+        title='Go To ScanAI'
+        onPress={() => navigation.navigate('ScanAI')}
+      />
+      <Button
+        title='Go To Login'
+        onPress={() => navigation.navigate('Login')}
       />
     </SafeAreaView>
   );
